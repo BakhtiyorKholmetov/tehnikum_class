b100ca0c1a761f6dc4639bf51bed1a691db1ed97
BakhtiyorKholmetov <75937401+BakhtiyorKholmetov@users.noreply.github.com>
2020-12-17 21:01:43 +0500

    Create Lesson_5.py

---
+++ class/Lesson_5.py
@@ -0,0 +1,12 @@
+a = []
+for i in range(10):
+    a.append(input())
+    print(i)
+print(a)
+
+
+summa = 0
+for i in range(10):
+    summa = summa + i
+    print('summa =', summa, 'i =', i)
+print(summa)
